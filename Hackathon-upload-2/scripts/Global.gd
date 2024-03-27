ef0dbe5d8d22401ba236b1a79220ca03fd39c085
Squirtlecon <97619754+Squirtlecon@users.noreply.github.com>
2024-03-26 21:33:44 -0700

    Ui done
    Hp, Mana, Energy bars
    (vars health, mana, nrg)

--- Hackathon-upload-2/scripts/Global.gd
+++ Hackathon-upload-2/scripts/Global.gd
@@ -7,7 +7,9 @@ var quest = false
 
 # Platformer Global Variables
 var flower_count = 0
-
+var nrg = 100
+var health = 100
+var mana = 100
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	quest = false
